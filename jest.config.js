39bee434fc9be02f8c8ae58e44add9a80c7fa2d8
niltonfernandesj <niltonfernandesj@gmail.com>
2025-10-30 01:44:59 -0300

    adiciona configurações adicionais para a suite de testes com `jest`

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+const nextJest = require("next/jest");
+
+dotenv.config({ path: ".env.development" });
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+module.exports = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
