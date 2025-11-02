1dc01ae2d1a22a64749e538f4360b162065ff17b
Nilton Fernandes <niltonfernandesj@gmail.com>
2025-11-02 14:27:55 -0300

    corrige bug de vazamento de conexões no endpoint `/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,34 +5,43 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const client = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
 
-  const defaultMigrationParameters = getDefaultMigrationParameters(client);
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationParameters);
-
-    client.end();
-
-    return response.status(200).json(pendingMigrations);
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).end();
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationParameters,
-      dryRun: false,
-    });
+  let client;
 
-    client.end();
+  try {
+    client = await database.getNewClient();
+    const defaultMigrationParameters = getDefaultMigrationParameters(client);
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(
+        defaultMigrationParameters,
+      );
+      return response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationParameters,
+        dryRun: false,
+      });
 
-  return response.status(405).end();
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 function getDefaultMigrationParameters(dbClient) {
