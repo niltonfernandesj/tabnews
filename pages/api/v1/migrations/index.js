0b4ad96767abe94e5a02899920a3b420ff709ffb
Nilton Fernandes <niltonfernandesj@gmail.com>
2025-11-02 14:21:11 -0300

    adiciona cenário com bug igual ao do curso no endpoint `/migration`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,40 +5,34 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  let client;
+  const client = await database.getNewClient();
 
-  try {
-    client = await database.getNewClient();
+  const defaultMigrationParameters = getDefaultMigrationParameters(client);
 
-    const defaultMigrationParameters = getDefaultMigrationParameters(client);
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationParameters);
 
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(
-        defaultMigrationParameters,
-      );
+    client.end();
 
-      return response.status(200).json(pendingMigrations);
-    }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationParameters,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-
-      return response.status(200).json(migratedMigrations);
-    }
-
-    return response.status(405).end();
-  } catch (error) {
-    throw error;
-  } finally {
-    await client.end();
+    return response.status(200).json(pendingMigrations);
   }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationParameters,
+      dryRun: false,
+    });
+
+    client.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
 }
 
 function getDefaultMigrationParameters(dbClient) {
