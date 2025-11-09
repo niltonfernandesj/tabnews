f84c9884a9ba63ce33cf3bf9b670e7657fd4ab0d
Nilton Fernandes <niltonfernandesj@gmail.com>
2025-11-09 19:46:00 -0300

    torna o comando `npm test` mais robusto

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,13 +1,16 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
+beforeAll(async () => {
+  await orchestrator.waitForWebServer();
+  await cleanDatabase();
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
