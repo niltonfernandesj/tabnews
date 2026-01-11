2441c7781ba40e0a9ab73355984f2f6ea90d1ae0
Nilton Fernandes <niltonfernandesj@gmail.com>
2026-01-11 16:19:02 -0300

    adds `Automated Tests` Github Actions workflow

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -12,7 +12,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.dependencies).toBeDefined();
   expect(responseBody.dependencies.database).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("21.0");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.active_connections).toEqual(1);
 });
